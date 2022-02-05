92d34be2626a491cd8acea409e68fd63317d7d9f
ryo0510 <3203ryo@gmail.com>
2022-02-05 06:42:47 +0000

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
    sessions: "admin/sessions"
   }
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :customers, only: [:show, :edit, :update]
+    resources :courses, except: [:destroy]
+    resources :categories, only: [:index, :create, :edit, :update]
+  end
 
   devise_for :customers,skip: [:passwords,], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  scope module: :public do
+    root to: "homes#top"
+    get 'homes/about' => 'homes#about'
+
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'customers_unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'customers_withdraw'
+    get 'customers/mypage' => 'customers#show'
+    get 'customers/mypage/edit' => 'customers#edit'
+    patch 'customers/update' => 'customers#update'
+
+    resources :post_messages, only: [:index, :new, :create, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :courses, only: [:index]
+    resources :entry_courses, only: [:index, :create, :update]
+    resources :course_results, only: [:show, :edit, :update]
+
+
+  end
 end
