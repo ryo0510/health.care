8f0b786c6ed15dffcb0dae821aea7b52ace6d110
ryo0510 <3203ryo@gmail.com>
2022-02-05 12:10:29 +0000

    cutomers関係追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,38 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_mypage_path, notice: "登録情報を更新しました"
+    else
+      flash.now[:alert] = "空欄があります。入力内容をご確認ください"
+      render "edit"
+    end
+  end
+    
+  def unsubscribe
+    @customer = current_customer
+  end
+  
+    #退会処理
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true) #falseからtrueにupdate
+    reset_session #ページ遷移しても以前入力した情報を保持することができる機能のことをsessionという
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :family_name_kana, :last_name_kana, :age, :image, :is_deleted)
+  end
+
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,15 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def customer_state # 退会しているかを判断するメソッド
+    @customer = Customer.find_by(email: params[:customer][:email]) ##入力されたemailからアカウントを1件取得
+    return if !@customer ## アカウントを取得できなかった場合、このメソッドを終了する
+    if (@customer.valid_password?(params[:customer][:password])) && (@customer.is_deleted == true)
+      flash[:notice] = "このアカウントは退会済みです。新規登録を行ってください。"
+      redirect_to new_customer_registration_path
+    end
+  end
 end
