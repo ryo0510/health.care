b9377bd2c37ed223c9d1253b346daf68cd582644
ryo0510 <3203ryo@gmail.com>
2022-02-06 07:01:03 +0000

    いいね非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,11 @@ class Public::FavoritesController < ApplicationController
     @post_message = PostMessage.find(params[:post_message_id])
     favorite = @post_message.favorites.new(customer_id: current_customer.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @post_message = PostMessage.find(params[:post_message_id])
     favorite = @post_message.favorites.find_by(customer_id: current_customer.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
