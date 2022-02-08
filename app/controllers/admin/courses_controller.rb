f69c35f914d9fa53a748ad0664183ab5ac2f476d
ryo0510 <3203ryo@gmail.com>
2022-02-08 06:31:08 +0000

    コース登録・編集機能追加

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,2 +1,48 @@
 class Admin::CoursesController < ApplicationController
+  before_action :ensure_course, only: [:show, :edit, :update]
+
+  def new
+    @course = Course.new
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      flash.now[:notice] = "コースを追加しました。"
+      redirect_to admin_course_path(@course)
+    else
+      flash.now[:alert] = "コースを追加できませんでした。"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      flash.now[:notice] = "編集内容を更新しました。"
+      redirect_to admin_course_path(@course)
+    else
+      flash.now[:alert] = "入力内容を確認してください。"
+      render :edit
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:category_id, :name, :working_item, :target_date, :way)
+  end
+
+  def ensure_course
+    @course = Course.find(params[:id])
+  end
 end
