96e3a03d3b26d4327ce068882b7c57c01eaade67
ryo0510 <3203ryo@gmail.com>
2022-02-14 05:51:49 +0000

    投稿ページレイアウト修正

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,8 @@ module HealthCare
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # 日本時間に指定
+    config.time_zone = 'Tokyo'
   end
 end
