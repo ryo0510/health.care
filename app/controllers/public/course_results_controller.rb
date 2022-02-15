6a95a638a17a4a8b769f087e195ee1d0a0f19fa5
ryo0510 <3203ryo@gmail.com>
2022-02-15 12:46:33 +0000

    ヘッダーレイアウト更新

--- app/controllers/public/course_results_controller.rb
+++ app/controllers/public/course_results_controller.rb
@@ -2,6 +2,6 @@ class Public::CourseResultsController < ApplicationController
   def show
     @course_result = CourseResult.find(params[:id])
     @history = History.new
-    @histories = @course_result.histories.order("start_time DESC")
+    @histories = @course_result.histories.order("start_time DESC").page(params[:page]).per(5)
   end
 end
