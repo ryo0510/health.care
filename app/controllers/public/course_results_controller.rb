7504bdee4bdb5b1ab8f4db674ce7ef8ea3d6fcba
ryo0510 <3203ryo@gmail.com>
2022-02-17 13:40:02 +0000

    各view修正

--- app/controllers/public/course_results_controller.rb
+++ app/controllers/public/course_results_controller.rb
@@ -3,5 +3,6 @@ class Public::CourseResultsController < ApplicationController
     @course_result = CourseResult.find(params[:id])
     @history = History.new
     @histories = @course_result.histories.order("start_time DESC").page(params[:page]).per(5)
+    Date.beginning_of_week = :sunday #日曜日から始まり(カレンダー)
   end
 end
