4fb44a2c7020321023fee31a855a02b7891c107a
ryo0510 <3203ryo@gmail.com>
2022-02-18 12:31:13 +0000

    ログイン,ログアウト遷移先変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -19,14 +19,16 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  protected
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   def customer_state # 退会しているかを判断するメソッド
     @customer = Customer.find_by(email: params[:customer][:email]) ##入力されたemailからアカウントを1件取得
