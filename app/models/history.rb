d081c5f08c35bbc9af441d935a858a07d1ac28d3
ryo0510 <3203ryo@gmail.com>
2022-02-19 11:15:22 +0000

    グラフに使用するメソッド変更

--- app/models/history.rb
+++ app/models/history.rb
@@ -5,14 +5,40 @@ class History < ApplicationRecord
   validates :customer_id, presence: true
   validates :course_result_id, presence: true
   validates :result, presence: true
-  validates :start_time, presence: true, uniqueness: { scope: :course_result } #1つのcourse_result_idでのみ適用
+  validates :start_time, presence: true, uniqueness: { scope: :course_result } #1つのcourse_result.idでのみ適用
 
-  scope :created_today, -> { find_by(start_time: Time.zone.now.midnight..Time.zone.now.end_of_day) } # 今日
-  scope :created_yesterday, -> { find_by(start_time: 1.day.ago.midnight..1.day.ago.end_of_day) } # 前日
-  scope :created_2day_ago, -> { find_by(start_time: 2.day.ago.midnight..2.day.ago.end_of_day) } # 2日前
-  scope :created_3day_ago, -> { find_by(start_time: 3.day.ago.midnight..3.day.ago.end_of_day) } # 3日前
-  scope :created_4day_ago, -> { find_by(start_time: 4.day.ago.midnight..4.day.ago.end_of_day) } # 4日前
-  scope :created_5day_ago, -> { find_by(start_time: 5.day.ago.midnight..5.day.ago.end_of_day) } # 5日前
-  scope :created_6day_ago, -> { find_by(start_time: 6.day.ago.midnight..6.day.ago.end_of_day) } # 6日前
+  def self.created_today
+    res = find_by(start_time: Time.zone.now.midnight..Time.zone.now.end_of_day)
+    res.nil? ? 0 : res.result
+  end
 
+  def self.created_yesterday
+    res = find_by(start_time: 1.day.ago.midnight..1.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
+
+  def self.created_2day_ago
+    res = find_by(start_time: 2.day.ago.midnight..2.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
+
+  def self.created_3day_ago
+    res = find_by(start_time: 3.day.ago.midnight..3.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
+
+  def self.created_4day_ago
+    res = find_by(start_time: 4.day.ago.midnight..4.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
+
+  def self.created_5day_ago
+    res = find_by(start_time: 5.day.ago.midnight..5.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
+
+  def self.created_6day_ago
+    res = find_by(start_time: 6.day.ago.midnight..6.day.ago.end_of_day)
+    res.nil? ? 0 : res.result
+  end
 end
