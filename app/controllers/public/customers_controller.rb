23a4a5abeaf2df52a28d3c62d8d1ff23002a6849
ryo0510 <3203ryo@gmail.com>
2022-02-24 12:16:26 +0000

    カラム名修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -39,7 +39,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :family_name_kana, :last_name_kana, :email, :tel, :address, :is_deleted)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :tel, :address, :is_deleted)
   end
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,6 @@ class Favorite < ApplicationRecord
   belongs_to :customer
   belongs_to :post_message
 
-  validates :post_message_id, uniqueness: { scope: :customer_id }
+  validates :customer_id, presence: true
+  validates :post_message_id, presence: true, uniqueness: { scope: :customer_id }
 end
