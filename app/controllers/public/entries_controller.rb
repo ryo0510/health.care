6830a2dd0863194d921207ef483463907c28c617
ryo0510 <3203ryo@gmail.com>
2022-02-27 08:13:30 +0000

    entriesコントローラ修正

--- app/controllers/public/entries_controller.rb
+++ app/controllers/public/entries_controller.rb
@@ -24,8 +24,8 @@ class Public::EntriesController < ApplicationController
     #データの一貫性を保つためトランザクション使用(どちらかの処理に異常があった場合、処理を中断しデータを保存しない)
     ActiveRecord::Base.transaction do
       converted_create_params.each do |course_id|
-        entry = current_customer.entries.create(course_id: course_id)
-        course = entry.course_results.create(entry_id: entry.id)
+        entry = current_customer.entries.create!(course_id: course_id)
+        course = entry.course_results.create!(entry_id: entry.id)
       end
     end
     redirect_to entries_path
