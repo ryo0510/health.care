612e508f30bb13e19fab0de44d74f13a7dc62b10
ryo0510 <3203ryo@gmail.com>
2022-03-07 14:49:59 +0000

    Historyモデルテストコード更新

--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :course do
     name { Faker::Lorem.characters(number: 5) }
     working_item { Faker::Lorem.characters(number: 30) }
-    target_date Faker::Number.between(from: 1, to: 120)
+    target_date { '85' }
     way { 'number' }
   end
 end
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe 'Historyモデルのテスト', type: :model do
     let(:entry) { create(:entry, customer_id: customer.id, course_id: course.id) }
     let(:course_result) { create(:course_result, entry_id: entry.id) }
     let!(:history) { build(:history, customer_id: customer.id, course_result_id: course_result.id) }
+    let!(:other_history) { create(:history, customer_id: customer.id, course_result_id: course_result.id) }
+
+    context 'customer_idカラム' do
+      it '空欄でないこと' do
+        history.customer_id = ''
+        expect(subject).to eq false
+      end
+    end
+
+    context 'course_result_idカラム' do
+      it '空欄でないこと' do
+        history.course_result_id = ''
+        expect(subject).to eq false
+      end
+    end
 
     context 'resultカラム' do
       it '空欄でないこと' do
@@ -26,6 +41,17 @@ RSpec.describe 'Historyモデルのテスト', type: :model do
       it '140文字以下であること: 141文字は×' do
         history.memo = Faker::Lorem.characters(number: 141)
         is_expected.to eq false
+      end
+    end
+
+    context 'start_timeカラム' do
+      it '空欄でないこと' do
+        history.start_time = ''
+        expect(subject).to eq false
+      end
+      it '一意性があること' do
+        history.start_time = other_history.start_time
+        expect(subject).to eq false
       end
     end
   end
