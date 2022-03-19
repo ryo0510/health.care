d755e453b16d61666ec98a91b00373300dc5ef12
ryo0510 <3203ryo@gmail.com>
2022-03-19 11:41:14 +0000

    Rspecテストコード追加

--- spec/system/customer_spec.rb
+++ spec/system/customer_spec.rb
@@ -9,6 +9,9 @@ describe '[STEP1] 会員ログイン前のテスト' do
     context 'リンクの内容を確認' do
       subject { current_path }
 
+      it 'URLが正しい' do
+        expect(current_path).to eq '/'
+      end
       it 'ロゴを押すと、トップ画面に遷移する' do
         expect(page).to have_link nil, href: root_path
         click_link nil, href: root_path
@@ -28,6 +31,51 @@ describe '[STEP1] 会員ログイン前のテスト' do
         expect(page).to have_link '全コース一覧', href: courses_path
         click_link '全コース一覧', href: courses_path
         is_expected.to eq '/courses'
+      end
+    end
+  end
+
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_customer_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/customers/sign_up'
+      end
+      it '「新規登録」と表示される' do
+        expect(page).to have_content '新規会員登録'
+      end
+      it 'last_nameフォームが表示される' do
+        expect(page).to have_field 'customer[last_name]'
+      end
+      it 'first_nameフォームが表示される' do
+        expect(page).to have_field 'customer[first_name]'
+      end
+      it 'last_name_kanaフォームが表示される' do
+        expect(page).to have_field 'customer[last_name_kana]'
+      end
+      it 'first_name_kanaフォームが表示される' do
+        expect(page).to have_field 'customer[first_name_kana]'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'customer[email]'
+      end
+      it 'telフォームが表示される' do
+        expect(page).to have_field 'customer[tel]'
+      end
+      it 'addressフォームが表示される' do
+        expect(page).to have_field 'customer[address]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'customer[password]'
+      end
+      it 'password_confirmationフォームが表示される' do
+        expect(page).to have_field 'customer[password_confirmation]'
+      end
+      it '新規登録ボタンが表示される' do
+        expect(page).to have_button '新規登録'
       end
     end
   end
