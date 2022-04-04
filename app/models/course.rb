16c35cc8b3d5791413589d3b4c3ea03b54bfadc8
ryo0510 <3203ryo@gmail.com>
2022-04-04 07:05:17 +0000

    エラーメッセージ2重で表示されるのを修正

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,7 +5,6 @@ class Course < ApplicationRecord
   enum way: { sign: 0, number: 1 }
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
-  validates :category_id, presence: true
   validates :working_item, presence: true
   validates :target_date, numericality: { only_integer: true, greater_than_or_equal_to: 1 } # 数値かつ、1以上
   validates :way, presence: true
