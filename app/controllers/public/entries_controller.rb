b9934c7506143162fa7f14e7b0f5cc7107d4eec2
ryo0510 <3203ryo@gmail.com>
2022-04-11 15:55:58 +0000

    entriesコントローラ修正

--- app/controllers/public/entries_controller.rb
+++ app/controllers/public/entries_controller.rb
@@ -18,12 +18,12 @@ class Public::EntriesController < ApplicationController
     end
 
     # 要素に空白がなぜかできてしまう為、reject使用し空白削除
-    converted_create_params = create_params[:course_id].reject { |v| v.empty? }.map(&:to_i)
+    converted_create_params = create_params[:course_id].compact.reject(&:empty?)
     # データの一貫性を保つためトランザクション使用(どちらかの処理に異常があった場合、処理を中断しデータを保存しない)
     ActiveRecord::Base.transaction do
       converted_create_params.each do |course_id|
-        entry = current_customer.entries.create!(course_id: course_id)
-        course = entry.course_results.create!(entry_id: entry.id)
+        entry = current_customer.entries.create(course_id: course_id)
+        course = entry.course_results.create(entry_id: entry.id)
       end
     end
     redirect_to entries_path
